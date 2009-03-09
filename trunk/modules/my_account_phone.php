a28e89850d3a2965c59d2a9dff64c7c45e4be05f
prophet.zoli <prophet.zoli@9059dc18-fa10-11dd-ac65-f74f249cfaac>
2009-03-09 15:49:14 +0000

    + prepisany kod a vzhlad v my_account (cez taby) - ak sa vymaze user, vymaze sa aj jeho phone - vymazane <head></head> v templates (naco to tam bolo? :)) ! vela oprav

--- trunk/modules/my_account_phone.php
+++ trunk/modules/my_account_phone.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	UCxml web Portal - Edit phone
+	UCxml web Portal - My Account
 
 	Zoli Toth, FEI TUKE
 	Unified Communications solution with Open Source applications - UCxml
@@ -10,89 +10,69 @@
 	Copyright (c) 2005, McFadden Associates.  All rights reserved.
 */
 
-
 //Checks if id is known, stores in variable
-if (isset($_GET['id_phone'])) $tmp_id_phone = defang_input($_GET['id_phone']);
+$tmp_id_user = defang_input($_SESSION['user_id']);
+$tmp_id_phone = defang_input($_SESSION['user_id']);
 
-if (isset($_POST['action']) || isset($_GET['submit_delete']))
+if (isset($_POST['action']))
 {
-	//User wants to save, cancel, or delete object
-
 	$myAction = defang_input($_POST['action']);
-	if ($myAction == "edit" || $_GET['submit_delete'] == yes)
+	if ($myAction == "edit")
 	{
-		if (isset($_POST['submit_save']))
+		if (isset($_POST['submit_save_phone']))
 		{
 			// Saving
 			$tmp_id_phone = defang_input($_POST['id_phone']);
-			$tmp_MAC = defang_input($_POST['MAC']);
 			$tmp_access_lvl = defang_input($_POST['access_lvl']);
-			$tmp_number = defang_input($_POST['number']);
 
 			$tmpUpdateSQL = "UPDATE phone SET
-				MAC = '$tmp_MAC',
-				number = '$tmp_number',
 				access_lvl = '$tmp_access_lvl'
 				WHERE id_phone ='$tmp_id_phone'";
 
-			if (mysql_query($tmpUpdateSQL, $db))
+			if(mysql_query($tmpUpdateSQL, $db))
 			{
-				header("Location: index.php?module=view_phones");
-			} else {
+				header("Location: index.php?module=my_account_phone");
+            } else {
 				echo "Unable to edit phone.";
 			}
 
-		} else if (isset($_POST['submit_delete']) || $_GET['submit_delete'] == 'yes') {
-			// Deleting
-			$tmp_id_phone = defang_input($tmp_id_phone);
-			delete_phone($tmp_id_phone);
-			header("Location: index.php?module=view_phones");
-
-		} else if (isset($_POST['submit_cancel'])) {
+		} else if (isset($_POST['submit_cancel_phone'])) {
 			// Cancel
-			if ($_GET['new'] == "true")
-			{
-
-				delete_phone($tmp_id_phone);
-			}
-			header("Location: index.php?module=view_phones");
+			header("Location: index.php?module=menu");
 		} else {
 			// Action, but no valid submit button.
-			header("Location: index.php?module=view_phone");
+			header("Location: index.php?module=my_account");
 		}
 
 	} else {
 		// Bad action
-		header("Location: index.php?module=view_phone");
+		header("Location: index.php?module=my_account");
 	}
-	
+
 } else {
 	// NO action
 	render_HeaderFooter("UCxml web Portal - Edit Phone Registrations");
-	output_edit_phone($tmp_id_phone);
+	output_edit_user($tmp_id_phone);
 	render_Footer();
 }
 
-function delete_phone ($tmp_id_phone)
-{
-	$sql = "DELETE FROM phone WHERE id_phone='$tmp_id_phone'";
-	$result = mysql_query($sql);
-}
-
 //Create page and fill in known data
-function output_edit_phone ($myID_phone)
+function output_edit_user ($myID_phone)
 {
 	include "language/lang.php";
-	global $db;
-	$xtpl=new XTemplate ("modules/templates/edit_phone.html");
+	global $db, $xtpl;
+	$xtpl=new XTemplate ("modules/templates/my_account_phone.html");
 	$xtpl->assign( 'LANG', $lang );
 
-	$theSQL = "SELECT * FROM phone WHERE id_phone='$myID_phone'";
+	$theSQL = "SELECT id_phone, nick, number, MAC, access_lvl FROM phone WHERE id_phone='$myID_phone'";
 	$theRES = mysql_query($theSQL, $db);
 	if ($in = mysql_fetch_assoc($theRES))
 	{
 		$xtpl->assign("id_phone",$in['id_phone']);
+		$xtpl->assign("nick",$in['nick']);
+   		$xtpl->assign("number",$in['number']);
 		$xtpl->assign("MAC",$in['MAC']);
+
 		if ($in['access_lvl'] == "Restricted")
 		{
 			$xtpl->assign("selected_restricted","selected");
@@ -110,14 +90,10 @@ function output_edit_phone ($myID_phone)
 			$xtpl->assign("selected_unrestricted","");
 			$xtpl->assign("selected_unknown","selected");
 		}
-		$xtpl->assign("number",$in['number']);
-		$xtpl->assign("nick",$in['nick']);
 
 	}
 	// Output
 	$xtpl->parse("main");
 	$xtpl->out("main");
 }
-
-
 ?>
